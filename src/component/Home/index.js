20f73ace8b4cfa370319384ae1596d99b3dc0ccb
Muhammad Hassan <muhhassan199@gmail.com>
2023-02-14 18:44:01 +0500

    Added image and layout set

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -1,19 +1,20 @@
 import DownloadCV from '../..';
+import Portrait from '../../assets/images/potrait.png';
 import '../Home/index.scss'
-import Portfolioimg from '../../assets/images/logo-scc.png'
 const Home = () => {
     return(
-    <div className='text-forhome'>
-
+    <div className='container home-page'>
+        <img src={Portrait} alt=''></img>
+        <div className='text-zone'>
         <h1>Hi,</h1>
         <h2>Hassan Here!</h2>
         <p>Welcome to my portfolio world! You can see my work and <br></br> if you want me to hire me download my CV below.</p>
-         <DownloadCV />
-         <div className='img-forhome'>
-        <img src={Portfolioimg} alt=''></img>
-        </div>        
+        <DownloadCV />
+        </div>
+                
         
     
+        
     </div>
 )
 }
